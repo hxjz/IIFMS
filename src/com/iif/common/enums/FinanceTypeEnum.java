b494bb437f8120a9b726dd4afefbed575532aa54
Gang <3538739181@qq.com>
2017-06-09 01:01:18 +0800

    Up By Gang: excel style

--- src/com/iif/common/enums/FinanceTypeEnum.java
+++ src/com/iif/common/enums/FinanceTypeEnum.java
@@ -12,12 +12,18 @@ import java.util.LinkedHashMap;
  * @Version: V1.0
  */
 public class FinanceTypeEnum {
-    static final public int TYPE1 = 1;// TEMP
-    static final public int TYPE2 = 2;// TEMP
-    static final public int TYPE3 = 3;// TEMP
-    static final public int TYPE4 = 4;// TEMP
-    static final public int TYPE5 = 5;// TEMP
-    static final public int TYPE6 = 6;// TEMP
+    static final public int TYPE1 = 1;// 手印痕迹
+    static final public int TYPE2 = 2;// 足迹痕迹
+    static final public int TYPE3 = 3;// 工具痕迹
+    static final public int TYPE4 = 4;// 文件痕迹
+    static final public int TYPE5 = 5;// 枪弹痕迹
+    static final public int TYPE6 = 6;// 特殊痕迹
+    static final public int TYPE7 = 7;//  理化物证
+    static final public int TYPE8= 8;//  生物物证
+    static final public int TYPE9 = 9;// 电子物证
+    static final public int TYPE10 = 10;// 视听物证
+    static final public int TYPE11 = 11;// 毒化物证
+
 
     // 此名固定，不可修改
     @SuppressWarnings("rawtypes")
--- src/com/iif/finances/web/ExportUtil.java
+++ src/com/iif/finances/web/ExportUtil.java
@@ -1,11 +1,10 @@
 package com.iif.finances.web;
 
 import com.hxjz.common.utils.DateUtil;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.util.RegionUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
@@ -24,11 +23,11 @@ import java.util.List;
  * @Version: V1.0
  */
 public class ExportUtil {
-    public static void exportStatistics(HttpServletRequest request, List data) {
+    public static void exportStatistics(HttpServletRequest request, List datas) {
         String templatePath = request.getSession().getServletContext().getRealPath("/template/财物统计表.xls");
         String date = DateUtil.getDateTime(DateUtil.DATE_FORMAT, new Date());
         String targetPath = request.getSession().getServletContext().getRealPath("/export/");
-        String fileName = "财务统计表_" + date + ".xls";
+        String fileName = "财物统计表_" + date + ".xls";
         HSSFWorkbook workbook = null;    //读取excel模板
         try {
             workbook = new HSSFWorkbook(new FileInputStream(templatePath));
@@ -36,7 +35,8 @@ public class ExportUtil {
                 HSSFSheet sheet = workbook.getSheetAt(0);   //读取第一个工作簿
                 HSSFRow row;
                 HSSFCell cell = null;
-                int rownum = 3; //添加的起始行
+                HSSFCellStyle style = setCellStyle(workbook);
+                int rowNum = 3; //添加的起始行
                 row = sheet.getRow(1);  // 从第二行开始写数据
                 cell = row.getCell(1);
                 cell.setCellValue("单位：北京市公安局");
@@ -44,32 +44,41 @@ public class ExportUtil {
                 row = sheet.getRow(4);  // 第5行
                 cell = row.getCell(1);
                 cell.setCellValue("统计时间：" + DateUtil.getDateTime(DateUtil.TIME_FORMAT, new Date()));
+                sheet.addMergedRegion(new CellRangeAddress(rowNum, rowNum, 1, 3));
 
                 row = sheet.getRow(5);  // 第6行
                 cell = row.getCell(2);
-                cell.setCellValue(data.size());
+                cell.setCellValue(datas.size());
                 cell = row.getCell(5);
                 cell.setCellValue("admin");
                 cell = row.getCell(8);
                 cell.setCellValue(DateUtil.getDateTime(DateUtil.TIME_FORMAT, new Date()));
 
 
-                int rowNum = 7; // 第8行
-                row = sheet.getRow(7);
-                CellStyle style = row.getRowStyle();
-                if (data.size() != 0) {
-                    for (int i = 0; i < data.size(); i++) {
-                        HashMap result = (HashMap) data.get(i);
-
+                rowNum = 7; // 第8行
+                if (datas.size() != 0) {
+                    for (Object data : datas) {
+                        HashMap result = (HashMap) data;
                         row = sheet.createRow(rowNum);
-                        cell = row.getCell(3);//财物类型
+                        cell = row.getCell(1);//财物类型
                         if (cell == null)
-                            cell = row.createCell(3);
+                            cell = row.createCell(1);
+                        cell.setCellStyle(style);
                         cell.setCellValue(result.get("typeName").toString());
-                        cell = row.getCell(6);//财务统计
+
+                        cell = row.getCell(4);//财物统计
                         if (cell == null)
-                            cell = row.createCell(6);
+                            cell = row.createCell(4);
+                        cell.setCellStyle(style);
                         cell.setCellValue(Integer.parseInt(result.get("total").toString()));
+
+                        // 合并单元格 处理
+                        int []columns=new int[]{1,4,7};
+                        for (int column : columns) {
+                            CellRangeAddress cra = new CellRangeAddress(rowNum, rowNum, column, column+2); // 起始行, 终止行, 起始列, 终止列
+                            sheet.addMergedRegion(cra);
+                            setMergedRegionStyle(cra,sheet,workbook);
+                        }
 
                         rowNum++;
                     }
@@ -97,11 +106,32 @@ public class ExportUtil {
         }
     }
 
+    private static HSSFCellStyle setCellStyle(HSSFWorkbook workbook) {
+        HSSFCellStyle style = workbook.createCellStyle(); // 样式对象
+        style.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);// 垂直
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER);// 水平
+        Font font = workbook.createFont();
+        font.setFontName("黑体");//字体类型
+        font.setFontHeightInPoints((short) 10);
+        style.setFont(font);
+//        style.setBorderBottom(HSSFCellStyle.BORDER_MEDIUM);//下边框类型
+//        style.setBorderLeft(HSSFCellStyle.BORDER_MEDIUM);//左边框类型
+//        style.setBorderRight(HSSFCellStyle.BORDER_MEDIUM);//右边框类型
+//        style.setBorderTop(HSSFCellStyle.BORDER_MEDIUM);//上边框类型
+        return style;
+    }
+
+    private static void setMergedRegionStyle(CellRangeAddress cra, HSSFSheet sheet, HSSFWorkbook workbook) {
+        RegionUtil.setBorderBottom(2, cra, sheet, workbook); // 下边框
+        RegionUtil.setBorderLeft(2, cra, sheet, workbook); // 左边框
+        RegionUtil.setBorderRight(2, cra, sheet, workbook); // 有边框
+        RegionUtil.setBorderTop(2, cra, sheet, workbook); // 上边框
+    }
 
     public static void main(String[] args) {
         String templatePath = "e:\\财物统计表.xls";
-        Date date = new Date();
-        String targetPath = "e:\\export\\财务统计表" + date.getTime() + ".xls";
+        String date = DateUtil.getDateTime(DateUtil.DATE_FORMAT, new Date());
+        String targetPath = "e:\\财物统计表_" + date + ".xls";
         HSSFWorkbook workbook = null;    //读取excel模板
         try {
             workbook = new HSSFWorkbook(new FileInputStream(templatePath));
@@ -109,7 +139,7 @@ public class ExportUtil {
                 HSSFSheet sheet = workbook.getSheetAt(0);   //读取第一个工作簿
                 HSSFRow row;
                 HSSFCell cell = null;
-                int rownum = 3; //添加的起始行
+                int rowNum = 3; //添加的起始行
                 row = sheet.getRow(1);  // 从第二行开始写数据
                 cell = row.getCell(1);
                 cell.setCellValue("单位：北京市公安局");
@@ -126,48 +156,47 @@ public class ExportUtil {
                 cell = row.getCell(8);
                 cell.setCellValue(DateUtil.getDateTime(DateUtil.TIME_FORMAT, new Date()));
 
-
-                int rowNum = 7; // 第8行
+                rowNum = 7; // 第8行
+                HSSFCellStyle newstaye = setCellStyle(workbook);
                 row = sheet.getRow(7);
-                int a = row.getFirstCellNum();
-                int b = row.getRowNum();
-                System.out.println(a);
-                System.out.println(b);
-                CellStyle style = row.getRowStyle();
-                for (int i = 0; i < 3; i++) {
+//                CellStyle style = row.getRowStyle();
+                for (int i = 0; i < 5; i++) {
 
                     row = sheet.getRow(rowNum);
-                    row.setRowStyle(style);
-                    cell = row.getCell(3);//财物类型
-                    if (cell == null) {
-                        cell = row.createCell(3);
+                    if (row == null) {
+                        row = sheet.createRow(rowNum);
                     }
 
-                    cell.setCellValue("足印痕迹");
-                    cell = row.getCell(6);//财务统计
+                    cell = row.createCell((short) 1);
+
+                    cell = row.getCell(1);//财物类型
                     if (cell == null) {
-                        cell = row.createCell(6);
+                        cell = row.createCell(1);
                     }
-                    cell.setCellValue("2");
+                    cell.setCellValue("足印痕迹" + i);
+
+                    cell = row.getCell(4);//财物统计
+                    if (cell == null) {
+                        cell = row.createCell(4);
+                    }
+                    cell.setCellValue("2" + i);
+                    // 合并单元格
+                    CellRangeAddress cra = new CellRangeAddress(rowNum, rowNum, 1, 3); // 起始行, 终止行, 起始列, 终止列
+                    sheet.addMergedRegion(cra);
+
+                    sheet.addMergedRegion(new CellRangeAddress(rowNum, rowNum, 1, 3));
+                    sheet.addMergedRegion(new CellRangeAddress(rowNum, rowNum, 4, 6));
+                    sheet.addMergedRegion(new CellRangeAddress(rowNum, rowNum, 7, 9));
+                    RegionUtil.setBorderBottom(2, cra, sheet, workbook); // 下边框
+                    RegionUtil.setBorderLeft(2, cra, sheet, workbook); // 左边框
+                    RegionUtil.setBorderRight(2, cra, sheet, workbook); // 有边框
+                    RegionUtil.setBorderTop(2, cra, sheet, workbook); // 上边框
                     rowNum++;
                 }
 
 
-//                if(data.size()!=0){
-//                    for(int i=0;i<data.size();i++){
-//                        HashMap result= (HashMap) data.get(i);
-//
-//                        row=sheet.getRow(rowNum);
-//                        cell = row.getCell((short)1);//财物类型
-//                        cell.setCellValue(result.get("financeType").toString());
-//                        cell = row.getCell((short)2);//财务统计
-//                        cell.setCellValue(Integer.parseInt(result.get("sum").toString()));
-//                        rowNum++;
-//                    }
-//                }
-
                 // 生成excel
-                File file = new File("");
+                File file = new File(targetPath);
                 if (file.exists() == false) {
                     file.createNewFile();
                 }
