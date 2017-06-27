f6ba4593aa38799821b538e1b275a56a18509244
Tomorrow <lmer08@qq.com>
2017-06-27 18:00:37 +0800

    Add By LiuM 2017.6.27

--- src/com/iif/storage/service/IStorageService.java
+++ src/com/iif/storage/service/IStorageService.java
@@ -1,9 +1,6 @@
 package com.iif.storage.service;
 
-import java.util.List;
-
 import com.hxjz.common.core.orm.IBaseService;
-import com.iif.storage.entity.Storage;
 
 public interface IStorageService extends IBaseService{
 	
