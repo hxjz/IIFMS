a3616383e967f32bd729a65782dbc1a75699a755
lmer08 <lmer08@qq.com>
2017-07-10 23:14:24 +0800

    Upd By LiuM

--- src/com/iif/storage/web/StorageAction.java
+++ src/com/iif/storage/web/StorageAction.java
@@ -92,7 +92,7 @@ public class StorageAction extends BaseAction{
 	 * @return
 	 */
 	@RequestMapping("toChooseStorage.action")
-	public String toChooseStorage() {
+	public String toEditStorage() {
 	    // 原始的数据
 	    List<Storage> rootStorage = storageService.findAll();
 	
@@ -118,58 +118,17 @@ public class StorageAction extends BaseAction{
 
         HttpTool.setAttribute("storageList", storageList);
 	    
-	    return "jsp/storage/chooseStorage";
+	    return "jsp/storage/editStorage";
 	
 	}
-	
-	/**
-	 * 递归查找子菜单
-	 * 
-	 * @param id
-	 *            当前菜单id
-	 * @param rootStorage
-	 *            要查找的列表
-	 * @return
-	 */
-	private List<Storage> getChild(String key, List<Storage> rootStorage) {
-	    // 子菜单
-	    List<Storage> childList = new ArrayList();
-	    for (Storage storage : rootStorage) {
-	        // 遍历所有节点，将父菜单id与传过来的id比较
-	        if (StringUtils.isNotBlank(storage.getParentId())) {
-	            if (storage.getParentId().equals(key)) {
-	                childList.add(storage);
-	            }
-	        }
-	    }
-	    // 把子菜单的子菜单再循环一遍
-	    for (Storage storage : childList) {// 没有Avail子菜单还有子菜单
-	        if (3 == storage.getIsAvail()) {
-	            // 递归
-	            storage.setChild(getChild(storage.getId(), rootStorage));
-	        }
-	    } // 递归退出条件
-	    if (childList.size() == 0) {
-	        return null;
-	    }
-	    return childList;
-	}
-
-	////////////////////////////////////////
-	//
-	//
-	//
-	////////////////////////////////////////
-	
-	
 	
 	/**
 	 * 添加枚举信息
 	 * @param dic Storage实体
 	 * @return 提示信息
 	 */
-	@RequestMapping("addStorage.action")
-	public void addStorage(Storage dic){
+	@RequestMapping("saveStorage.action")
+	public void saveStorage(Storage dic){
 		dic.setCreateTime(new Date());
 		dic.setUpdateTime(new Date());
 		dic.setIsDel(0);
